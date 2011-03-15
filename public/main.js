3e01073deae078d6af6d4a996a8f7e45237ef8cb
David Grayson <davidegrayson@gmail.com>
2011-03-14 22:33:45 -0700

    First commit.

---
+++ public/main.js
@@ -0,0 +1,214 @@
+var ws;
+var connected;
+var userName;
+
+// Called when the document has been loaded.
+function start()
+{
+		if (!("WebSocket" in window))
+		{
+				status(false, "Your browser does not support web sockets.  Please use <a href='http://www.google.com/chrome'>Google Chrome</a>.");
+				return;
+		}
+		
+		status(false, "Trying to connect.");
+
+		var name = getUrlParameter('name');
+		
+		ws = new WebSocket("ws://"+window.location.hostname+":8080/chat?name=" + escape(name));
+		ws.onopen = onOpen;
+		ws.onmessage = onMessage;
+		ws.onclose = onClose;
+
+    sendKeepAlives();
+}
+
+function sendKeepAlives()
+{
+		if (ws && ws.readyState == WebSocket.OPEN)  // If a websocket is open...
+    {
+        ws.send('');  // Send an empty string to avoid TCP timeout.
+		}
+    setTimeout("sendKeepAlives();", 60000);
+}
+
+function onClose()
+{
+		status(false, "Connection closed.");
+}
+
+function onOpen()
+{
+		// Web Socket is connected. You can send data by send() method.
+    userName = undefined;
+	  status(true, "Connected.");
+}
+
+function onMessage(evt)
+{
+		if (evt.data == "")
+		{
+				// Received a keepalive message from the host.
+				return;
+		}
+
+		// The first character is the command.  The rest is
+    // the data.
+	  var command = evt.data[0]
+		var data = evt.data.slice(1)
+
+		if (command=='c')
+		{
+				// Received a message for the chat room.
+				chatView.add("<div class=\"chat_message\">"+sanitize(data)+"</div>");
+		}
+		else if (command=='e')
+		{
+				// Received a notification that a new user has arrived.
+				chatView.add("<div class=\"chat_message\">" + data + " has entered.</div>");
+				userList.add(data);
+		}
+		else if (command=='l')
+		{
+				// Received a notification that a user has left.
+				chatView.add("<div class=\"chat_message\">" + data + " has left.</div>");
+				userList.remove(data);
+		}
+		else if (command == 's')
+		{
+				// Received the current state of the chat room.
+				// Currently all this has is a list of the particpants.
+				var names = data.split(',')
+				for (var i=0; i < names.length; i++)
+				{
+						userList.add(names[i])
+				}
+		}
+		else if (command == 'n')
+		{
+				// Received the name that has been assigned to us by the server.
+        userName = data;
+        updateServerStatus();
+		}
+}
+
+function updateServerStatus()
+{
+    document.body.className = connected ? "connected" : "not_connected";
+    serverStatus = document.getElementById("server_status");
+		if (connected)
+    {
+        if (userName)
+        {
+            serverStatus.innerHTML = "Logged in as <b>"+userName+".</b>";
+        }
+        else
+        {
+            serverStatus.innerHTML = "Connected.";
+        }
+    }
+    else
+    {
+        serverStatus.innerHTML = "Not connected.";
+    }
+}
+
+function status(new_connected, str)
+{
+		if (new_connected != connected)
+		{
+				connected = new_connected;
+				updateServerStatus();
+    }
+		chatView.add("<div class=\"status_message\">"+str+"</div>");
+}
+
+userList = {};
+userList.docObj = document.getElementById("chat_user_list");
+userList.names = [];
+userList.elementFor = function(name)
+{
+    return document.getElementById('user_list_item_'+name);
+}
+userList.add = function(name)
+{
+    // Create the element.
+    var new_element = document.createElement('div');
+		new_element.setAttribute('id', "user_list_item_"+name)
+    new_element.appendChild(document.createTextNode(name)); 
+
+    // Insert it in the correct place in the userList.names
+    // list and in the DOM. 
+		var i;
+    for(i = 0; i < this.names.length; i++)
+    {
+				if (this.names[i] > name)
+        {
+						var b = this.elementFor(this.names[i]);
+						this.docObj.insertBefore(new_element, b);
+            this.names.splice(i,0,name);
+						return;
+        }
+		}
+
+		this.names.push(name);
+    this.docObj.appendChild(new_element);
+}
+userList.remove = function(name)
+{
+		var uli = this.elementFor(name);
+    if (uli)
+		{
+				userList.docObj.removeChild(uli);
+		}
+
+    var i;
+    for (i = 0; i < this.names.length; i++)
+    {
+				if (this.names[i] == name)
+        {
+            this.names.splice(i,1);
+        }
+    }
+}
+		
+var chatView = document.getElementById("chatView");
+chatView.add = function(str)
+{
+    var scrollIsAtBottom = (this.scrollTop >= this.scrollHeight - this.clientHeight - 10);
+		this.innerHTML += str;
+		
+		if (scrollIsAtBottom)
+    {
+        this.scrollToBottom();
+		}
+}
+chatView.scrollToBottom = function()
+{
+    this.scrollTop = this.scrollHeight - this.clientHeight;
+}
+
+function sendChat()
+{
+		var chat_input = document.getElementById('chat_input');
+		var val = chat_input.value;
+		if (val!='')
+		{
+				ws.send('c'+val);
+				chat_input.value = '';
+        chatView.scrollToBottom();
+		}
+}
+
+function getUrlParameter(name)
+{
+		var regexS = "[\\?&]"+name+"=([^&#]*)";
+		var regex = new RegExp(regexS);
+		var results = regex.exec(window.location.href);
+		return results == null ? "" : results[1];
+}
+
+function sanitize(str)
+{
+		return str.replace(/</g,"&lt;").replace(/>/g,"&gt;");
+}
---
+++ rws1/server.rb
@@ -0,0 +1,91 @@
+require 'em-websocket'
+
+class Client
+  attr_reader :websocket
+  attr_accessor :name
+
+  def initialize(websocket_arg)
+    @websocket = websocket_arg
+  end
+end
+
+class ChatRoom
+  MaxNameLength = 15
+
+  attr_accessor :clients
+
+  def initialize
+    @clients = {}
+  end
+
+  def start(opts={})
+    EventMachine::WebSocket.start(opts) do |websocket|
+      websocket.onopen    { add_client(websocket) }
+      websocket.onmessage { |msg| handle_message(websocket, msg) }
+      websocket.onclose   { remove_client(websocket) }
+    end
+  end
+
+  def add_client(websocket)
+    client = Client.new(websocket)
+    client.name = assign_name(websocket.request["Query"]["name"])
+    send_all "e" + client.name                   # Alert other clients.
+    @clients[websocket] = client
+    websocket.send "n" + client.name             # Tell client what its assigned name is.
+    websocket.send "s" + client_names.join(",")  # Tell client who is in the room.
+  end
+
+  def remove_client(websocket)
+    client = @clients[websocket]
+    @clients.delete(websocket)
+    send_all "l" + client.name      # Alert other clients.
+  end
+
+  # Sends a message (UTF-8 websocket frame) to all clients.
+  def send_all(message)
+    @clients.each do |websocket, client|
+      websocket.send message
+    end
+    puts "send_all: #{message}"
+  end
+
+  # Handle a message (UTF-8 websocket frame) received from a websocket.
+  def handle_message(websocket, message)
+    return if message == ""
+    command, data = "command_#{message[0]}", message[1..-1]
+    if respond_to?(command)
+      send(command, websocket, data)
+    end
+  end
+
+  # This is called when we receive a message beginning with "c".
+  # These messages are chat messages, so we send them to all clients.
+  def command_c(websocket, chat_message)
+    send_all "c#{@clients[websocket].name}: #{chat_message}"
+  end
+
+  def client_names
+    @clients.collect{|websocket, c| c.name}.sort
+  end
+
+  def sanitize_user_name(raw_name)
+    name = raw_name.to_s.scan(/[[:alnum:]]/).join[0,MaxNameLength]
+    name.empty? ? "Guest" : name
+  end
+
+  def assign_name(requested_name)
+    name = sanitize_user_name(requested_name)
+    existing_names = self.client_names
+    if existing_names.include?(name)
+      i = 2
+      while existing_names.include?(name + i.to_s)
+        i += 1
+      end
+      name += i.to_s
+    end
+    return name
+  end
+end
+
+chatroom = ChatRoom.new
+chatroom.start(:host=>"", :port=>8080)
